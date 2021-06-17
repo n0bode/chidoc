73e8f4ea21cca2de13eb025cc64f3aa2a42f60d2
Paulo Camacan <paulo.camacan@lucree.com.br>
2021-06-17 14:14:38 -0300

    add modules image scaled

--- image.go
+++ image.go
@@ -4,6 +4,8 @@ import (
 	"image"
 	"image/png"
 	"net/http"
+
+	"golang.org/x/image/draw"
 )
 
 // HandlerImage is a handle to get a png image
@@ -24,5 +26,30 @@ func ImageFromURL(url string) HandlerImage {
 			return image.Black
 		}
 		return img
+	}
+}
+
+// ImageFromURL return handler from a URL
+// URL must be returns a png
+func ImageFromURLScaled(url string, scale float32) HandlerImage {
+	return func() image.Image {
+		resp, err := http.Get(url)
+		if err != nil {
+			return image.Black
+		}
+		defer resp.Body.Close()
+
+		img, err := png.Decode(resp.Body)
+		if err != nil {
+			return image.Black
+		}
+
+		rw := int(float32(img.Bounds().Max.X) * scale)
+		rh := int(float32(img.Bounds().Max.Y) * scale)
+
+		dst := image.NewRGBA(image.Rect(0, 0, rw, rh))
+
+		draw.ApproxBiLinear.Scale(dst, dst.Rect, img, img.Bounds(), draw.Over, nil)
+		return dst
 	}
 }
